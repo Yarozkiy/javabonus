c88fe7b385b08db8c81d41d4bed27cbc0b17d13c
Яроцкий Алексей Влдаимирович <aleksey.yarozkiy@yandex.ru>
2022-05-27 18:05:30 +0200

    Add project bonus

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 999_992;
+        int percent = 20;
+        int bonus = ticket / percent;
+        System.out.println("Dear passenger!");
+        System.out.println("Сongratulations you have accumulated: "+ bonus+" bonus miles");
+        System.out.println("Thank you for choosing our airline!");
+    }
+}
